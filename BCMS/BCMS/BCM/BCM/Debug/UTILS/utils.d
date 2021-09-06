9b5a88434f6d97f08974ae0d51b7a5e16fee30df
MohamedAyman19 <87302157+MohamedAyman19@users.noreply.github.com>
2021-09-06 13:59:44 +0200

    Delete BCMS/BCMS directory

--- BCMS/BCMS/BCM/BCM/Debug/UTILS/utils.d
+++
@@ -1,9 +0,0 @@
-UTILS/utils.d UTILS/utils.o: ../UTILS/utils.c ../UTILS/utils.h \
- ../UTILS/types.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
-
-../UTILS/utils.h:
-
-../UTILS/types.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
